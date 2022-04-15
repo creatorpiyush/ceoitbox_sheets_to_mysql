9dbd678d4361fe84ee9c6416e0403e2c14930439
creatorpiyush <piyushanand.1221@gmail.com>
2022-04-15 15:47:15 +0530

    some updates

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.get("/show-all-tables", (req, res) => {
   var sql = "show tables";
   db.query(sql, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 
@@ -25,7 +26,8 @@ app.post("/create-table", (req, res) => {
   var sql = `CREATE TABLE ${req.body.tableName} (id INT AUTO_INCREMENT PRIMARY KEY)`;
   db.query(sql, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 
@@ -34,7 +36,8 @@ app.delete("/delete-table/:tableName", (req, res) => {
   var sql = `DROP TABLE ${req.params.tableName}`;
   db.query(sql, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 
@@ -43,7 +46,8 @@ app.post("/alter-table/:tableName", (req, res) => {
   var sql = `ALTER TABLE ${req.params.tableName} ADD ${req.body.columnName} ${req.body.columnType}`;
   db.query(sql, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 
@@ -52,7 +56,8 @@ app.delete("/delete-column/:tableName", (req, res) => {
   var sql = `ALTER TABLE ${req.params.tableName} DROP COLUMN ${req.body.columnName}`;
   db.query(sql, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 
@@ -61,7 +66,8 @@ app.get("/table/:table_name", (req, res) => {
   var sql = `select * from ${req.params.table_name}`;
   db.query(sql, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 
@@ -71,7 +77,8 @@ app.post("/table/:table_name", (req, res) => {
   var sql = `insert into ${req.params.table_name} set ?`;
   db.query(sql, post, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 
@@ -80,7 +87,8 @@ app.delete("/table/delete/:table_name", (req, res) => {
   var sql = `delete from ${req.params.table_name} where ${req.body.toDeleteKey} = ${req.body.toDeleteValue}`;
   db.query(sql, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 
@@ -92,34 +100,39 @@ app.get("/select/:table_name", (req, res) => {
     var sql = `select * from ${req.params.table_name} where ${clause};`;
     db.query(sql, (err, result) => {
       if (err) throw err;
-      return res.status(200).send(result);
+      // return res.status(200).send(result);
+      return res.status(200).send("Successfully");
     });
   } else if (condition == "greater") {
     var clause = req.body.key + " > " + `'${req.body.value}'`;
     var sql = `select * from ${req.params.table_name} where ${clause};`;
     db.query(sql, (err, result) => {
       if (err) throw err;
-      return res.status(200).send(result);
+      // return res.status(200).send(result);
+      return res.status(200).send("Successfully");
     });
   } else if (condition == "less") {
     var clause = req.body.key + " < " + `'${req.body.value}'`;
     var sql = `select * from ${req.params.table_name} where ${clause};`;
     db.query(sql, (err, result) => {
       if (err) throw err;
-      return res.status(200).send(result);
+      // return res.status(200).send(result);
+      return res.status(200).send("Successfully");
     });
   } else if (condition == "like") {
     var clause = req.body.key + " like " + `'%${req.body.value}%'`;
     var sql = `select * from ${req.params.table_name} where ${clause};`;
     db.query(sql, (err, result) => {
       if (err) throw err;
-      return res.status(200).send(result);
+      // return res.status(200).send(result);
+      return res.status(200).send("Successfully");
     });
   } else {
     var sql = `select * from ${req.params.table_name};`;
     db.query(sql, (err, result) => {
       if (err) throw err;
-      return res.status(200).send(result);
+      // return res.status(200).send(result);
+      return res.status(200).send("Successfully");
     });
   }
 });
@@ -129,7 +142,8 @@ app.put("/table/update/:table_name", (req, res) => {
   var sql = `update ${req.params.table_name} set ? where id = ${req.body.id}`;
   db.query(sql, req.body, (err, result) => {
     if (err) throw err;
-    return res.status(200).send(result);
+    // return res.status(200).send(result);
+    return res.status(200).send("Successfully");
   });
 });
 

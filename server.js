65da0f7bcfea209601a83ca782967513bb177ccd
creatorpiyush <piyushanand.1221@gmail.com>
2022-04-15 14:52:01 +0530

    some updates

--- server.js
+++ server.js
@@ -89,6 +89,8 @@ app.get("/select/:table_name", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
